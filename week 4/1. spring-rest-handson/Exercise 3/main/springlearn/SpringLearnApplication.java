1b04eabd7a92af99dab5083493300f493f58e2a9
Vishwasgowdaba <123919759+Vishwasgowdaba@users.noreply.github.com>
2025-07-15 22:23:47 +0530

    week 4 completed

---
+++ week 4/1. spring-rest-handson/Exercise 3/main/springlearn/SpringLearnApplication.java
@@ -0,0 +1,32 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        LOGGER.info("START MAIN");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        displayDate();
+        LOGGER.info("END MAIN");
+    }
+
+    public static void displayDate() throws Exception {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        Date date = format.parse("31/12/2018");
+        LOGGER.debug("Parsed Date: {}", date);
+        LOGGER.info("END");
+    }
+}
